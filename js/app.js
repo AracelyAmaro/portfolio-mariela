880a589dbc9c763eb023a78b4d5bb809cb4b3c64
AracelyAmaro <ara6amaro@gmail.com>
2022-01-31 02:24:19 -0300

    agrego responsive,hacer 1 media query mas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 //inicializo el aos
   AOS.init();
-
+  const boton_ = document.querySelector(".toggle");
+  const nav_bar = document.querySelector(".nav-bar-menu");
+ 
+  boton_.addEventListener ('click', () => {
+      nav_bar.classList.toggle('activado');
+    
+  })
