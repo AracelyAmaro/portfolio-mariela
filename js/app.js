d5ec553584cf23897074455c6d0b7609c62c2b91
AracelyAmaro <ara6amaro@gmail.com>
2022-02-03 17:11:45 -0300

    agrego modificaciones para diseño responsive

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@
   AOS.init();
   const boton_ = document.querySelector(".toggle");
   const nav_bar = document.querySelector(".nav-bar-menu");
- 
+  
+  const arrow = document.getElementById('#arrow');
+  const imagen_banner = document.getElementById('#img-ppal');
+  
   boton_.addEventListener ('click', () => {
       nav_bar.classList.toggle('activado');
     
+  })
+
+  arrow.addEventListener('click', () => {
+    // imagen_banner.innerHTML.
   })
